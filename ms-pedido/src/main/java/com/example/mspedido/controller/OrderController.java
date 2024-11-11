43f91ec7a6a0ca27157b6b573928c202e5bd41b6
brayanraul545 <brayanraulcondori@gmail.com>
2024-11-11 09:16:32 -0500

    FRONT

--- ms-pedido/src/main/java/com/example/mspedido/controller/OrderController.java
+++ ms-pedido/src/main/java/com/example/mspedido/controller/OrderController.java
@@ -1,8 +1,16 @@
 package com.example.mspedido.controller;
 
+import com.example.mspedido.dto.Client;
+import com.example.mspedido.dto.ErrorResponser;
+import com.example.mspedido.dto.Product;
 import com.example.mspedido.entity.Order;
+import com.example.mspedido.entity.OrderDetail;
+import com.example.mspedido.feign.ClientFeign;
+import com.example.mspedido.feign.ProductFeign;
 import com.example.mspedido.service.OrderService;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +22,11 @@ import java.util.Optional;
 public class OrderController {
     @Autowired
     private OrderService orderService;
+    @Autowired
+    private ClientFeign clientFeign;
+    @Autowired
+    private ProductFeign productFeign;
+
     @GetMapping
     public ResponseEntity<List<Order>> findAll() {
         return ResponseEntity.ok(orderService.findAll());
@@ -23,8 +36,23 @@ public class OrderController {
         return ResponseEntity.ok(orderService.findById(id));
     }
     @PostMapping
-    public ResponseEntity<Order> create(@RequestBody Order order) {
-        return ResponseEntity.ok(orderService.save(order));
+    public ResponseEntity<?> create(@RequestBody Order order) {
+        Client clientDto = clientFeign.getById(order.getClientId()).getBody();
+
+        if (clientDto == null || clientDto.getId() == null) {
+            String errorMessage = "Error: Cliente no encontrado.";
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponser(errorMessage));
+        }
+        for (OrderDetail orderDetail : order.getDetail()) {
+            Product product = productFeign.getById(orderDetail.getProductId()).getBody();
+
+            if (product == null || product.getId() == null) {
+                String errorMessage = "Error: producto no encontrado.";
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponser(errorMessage));
+            }
+        }
+        Order newOrder = orderService.save(order);
+        return ResponseEntity.ok(newOrder);
     }
     @PutMapping("/{id}")
     public ResponseEntity<Order> update(@PathVariable Integer id, @RequestBody Order order) {
---
+++ ms-pedido/src/main/java/com/example/mspedido/dto/ErrorResponser.java
@@ -0,0 +1,12 @@
+package com.example.mspedido.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponser {
+    private String message;
+}
